6664004c488b9f85ac544bada2c4c170effe20f2
Alejandro Ferrero <alejandroferrero@outlook.com>
2018-12-05 23:50:21 -0500

    Added remove button and description and applied css styling to them

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -9,6 +9,13 @@ class Photo extends Component {
     return (
       <figure className="figure">
         <img className="photo" src={post.imageLink} alt={post.description} />
+        <figcaption>
+          <p>{post.description}</p>
+          <div className="button-container" >
+            <button className="remove-button">Remove</button>
+          </div>
+          
+        </figcaption>
       </figure>
     );
   }
