71e0bb6b69c4e49846811bd545d9097aac48ad04
Alejandro Ferrero <alejandroferrero@outlook.com>
2018-12-06 23:08:58 -0500

    Turned Photo component into a stateless function

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -1,30 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class Photo extends Component {
-  render() {
-    const {
-      post,
-      onRemovePhoto,
-    } = this.props;
-    return (
-      <figure className="figure">
-        <img className="photo" src={post.imageLink} alt={post.description} />
-        <figcaption>
-          <p>{post.description}</p>
-          <div className="button-container">
-            <button
-              className="remove-button"
-              onClick={() => onRemovePhoto(post)}
-            >
-            Remove
-            </button>
-          </div>
-        </figcaption>
-      </figure>
-    );
-  }
-}
+const Photo = ({ post, onRemovePhoto }) => (
+  <figure className="figure">
+    <img className="photo" src={post.imageLink} alt={post.description} />
+    <figcaption>
+      <p>{post.description}</p>
+      <div className="button-container">
+        <button
+          className="remove-button"
+          onClick={() => onRemovePhoto(post)}
+        >
+        Remove
+        </button>
+      </div>
+    </figcaption>
+  </figure>
+);
 
 Photo.propTypes = {
   post: PropTypes.shape({
