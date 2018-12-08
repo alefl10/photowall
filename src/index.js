1d52203f038dd7256a77b9d57b9953d436055c2c
Alejandro Ferrero <alejandroferrero@outlook.com>
2018-12-08 15:33:53 -0500

    Added redux and basic store with reducer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import rootReducer from './redux/reducer';
 import App from './Components/App';
 import './styles/stylesheet.css';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
