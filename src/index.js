6602a88a5c1d91b92fd620428274cfb0271a00c2
Alejandro Ferrero <alejandroferrero@outlook.com>
2018-12-10 01:31:51 -0500

    Added Google Chrome Redux Developer Tools Extension to the Redux Store - Must Have extension installed on Google Chrome previously

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import rootReducer from './redux/reducer';
 import App from './Components/App';
 import './styles/stylesheet.css';
 
-const store = createStore(rootReducer);
+const ChromeReduxDevToolExt = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(rootReducer, ChromeReduxDevToolExt);
 
 ReactDOM.render(<Provider store={store}><BrowserRouter><App /></BrowserRouter></Provider>, document.getElementById('root'));
