64434d3ba371f265b4a1ed3ddd07fd2dfae520f7
Alejandro Ferrero <alejandroferrero@outlook.com>
2018-12-11 01:34:11 -0500

    Modified Photo Component to display comment count

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -7,8 +7,10 @@ const Photo = (props) => {
     post,
     index,
     removePost,
+    comments,
     history,
   } = props;
+  console.log(comments);
   return (
     <figure className="figure">
       <Link to={`/single/${post.id}`}><img className="photo" src={post.imageLink} alt={post.description} /></Link>
@@ -24,6 +26,12 @@ const Photo = (props) => {
           >
           Remove
           </button>
+          <Link className="button" to={`/single/${post.id}`}>
+            <div className="comment-count">
+              <div className="speech-bubble" />
+              {comments[post.id] ? comments[post.id].length : 0 }
+            </div>
+          </Link>
         </div>
       </figcaption>
     </figure>
@@ -36,6 +44,11 @@ Photo.propTypes = {
     id: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
     imageLink: PropTypes.string.isRequired,
+  }).isRequired,
+  comments: PropTypes.shape({
+    postId: PropTypes.arrayOf(
+      PropTypes.string.isRequired,
+    ),
   }).isRequired,
   removePost: PropTypes.func.isRequired,
 };
