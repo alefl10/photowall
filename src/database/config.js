f023bc7f9e7ddc065b97f8fa0d5fac0b1f3bb252
Alejandro Ferrero <alejandroferrero@outlook.com>
2018-12-13 20:47:05 -0500

    Added Firebase config

---
+++ src/database/config.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyDimcKYt6BKFp1QmQXqG0gpUT9bcN1F0Ao',
+  authDomain: 'photowall-42934.firebaseapp.com',
+  databaseURL: 'https://photowall-42934.firebaseio.com',
+  projectId: 'photowall-42934',
+  storageBucket: 'photowall-42934.appspot.com',
+  messagingSenderId: '816788436301',
+};
+
+firebase.initializeApp(config);
